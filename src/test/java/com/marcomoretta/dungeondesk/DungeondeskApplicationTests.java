160efcd25df2a430f01209d6eadec05198c86cc2
Omegamorello <omegamorello@gmail.com>
2026-07-17 18:52:57 +0200

    init: first commit

---
+++ src/test/java/com/marcomoretta/dungeondesk/DungeondeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcomoretta.dungeondesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeondeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
