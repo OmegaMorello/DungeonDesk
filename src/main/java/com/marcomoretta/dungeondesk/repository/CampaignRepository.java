5e9ce8330e7ba60104cbbd80371f648ba9aeeb14
Marco Moretta <marco@marcomoretta.com>
2026-07-31 23:32:34 +0200

    feat: add @EntityGraph to load needed lazy components

--- src/main/java/com/marcomoretta/dungeondesk/repository/CampaignRepository.java
+++ src/main/java/com/marcomoretta/dungeondesk/repository/CampaignRepository.java
@@ -1,12 +1,24 @@
 package com.marcomoretta.dungeondesk.repository;
 
 import com.marcomoretta.dungeondesk.domain.entity.Campaign;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Campaign persistency layer interface
  */
 @Repository
 public interface CampaignRepository extends JpaRepository<Campaign, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = {"players", "owner"})
+    List<Campaign> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = {"players", "owner"})
+    Optional<Campaign> findById(Long id);
 }
